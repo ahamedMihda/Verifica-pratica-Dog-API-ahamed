46b918f60446a8417d36fd7811f5b2a736a97ba6
student <student@itismeucci.com>
2026-05-15 12:35:22 +0200

    aggiunti variabili di razze

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-const allDogListURL = "https://dog.ceo/api/breed/hound/list"
+const allSubRaceogListURL = "https://dog.ceo/api/breeds/list/all"
+const afghan = document.querySelector("#afghan")
+const basset = document.querySelector("#basset")
+const blood = document.querySelector("#blood")
+const english = document.querySelector("#english")
+const ibizan = document.querySelector("#ibizan")
+
+
